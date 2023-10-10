1e4469f8da0ee6c0a2c4f2d005d11bbb41e60834
itbsp16 [Mega.Fatayat] <P041121007@bussan.co.id>
2023-10-10 16:01:52 +0700

    Add theme AdminLTE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    redirectTo: ''
+  },
+  { 
+    path: '', 
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
