3143e0ea4d5a8e2df6a464094984465e35a7b808
itbsp16 [Mega.Fatayat] <P041121007@bussan.co.id>
2023-10-12 07:08:53 +0700

    Feature - Implement angular datatables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidebarComponent } from './components/dashboard/sidebar/sidebar.component';
 import { ListComponent } from './components/dashboard/list/list.component';
 import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
+import { DataTablesModule } from "angular-datatables";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { NavbarComponent } from './components/dashboard/navbar/navbar.component'
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    DataTablesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dashboard/list/list.component.ts
+++ src/app/components/dashboard/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Employee } from '../../../models/employee.model';
 import { EmployeeService } from '../../../services/employee/employee.service';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -11,22 +12,27 @@ import { EmployeeService } from '../../../services/employee/employee.service';
 export class ListComponent implements OnInit {
   admin: boolean = false;
   @Input() employees: Employee[] = [];
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
 
   constructor(
     private readonly employeeService: EmployeeService,
-    private readonly router: Router,
+    @Inject(HttpClient) private httpClient: HttpClient
   ) { }
 
   ngOnInit(): void {
     this.isAdmin();
+    this.getAll();
     if (this.admin) {
-      this.getAll();
+      this.dtOptions = {
+        pagingType: 'full_numbers'
+      };
     }
   }
 
   isAdmin(): void {
     const user = sessionStorage.getItem('username');
-    if (user == "alFaqir") {
+    if (user == "admin") {
       this.admin = true;
     }
   }
@@ -35,6 +41,7 @@ export class ListComponent implements OnInit {
     this.employeeService.getAll().then(
       (resp) => {
         this.employees = resp;
+        this.dtTrigger.next(null);
       }
     );
   }
